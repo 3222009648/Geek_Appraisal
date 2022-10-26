d188aeed44ab46a431208b7c85c83b6e4a23181a
3222009648 <3222009648@mail2.gdut.edu.cn>
2022-10-26 16:57:38 +0800

    Add Project

--- task1/main.c
+++ task1/main.c
@@ -1,8 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char *argv[]) {
-	printf("Hello World!\n");
-	return 0;
-}
-
+#include <stdio.h> 
+int main(){
+         double h=0.001;
+         double a=1.0;
+         double y;
+         double z=a-y;
+         double t=0;
+         double w;
+         double m=1.0;
+         
+         while(z>0.001*h&&w<m){
+            y=a-a*h;
+             w=t+h;
+             t=w;
+             z=a-y;
+             a=y;
+             printf("%f  %f   %f\n",w,y,z);
+             }
+         printf("%f   %f\n",y,w);
+    return 0;
+    }
